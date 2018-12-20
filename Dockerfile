d1cef1fa93b26d9d10bdfd744c7352babc27b06b
pilillo <pilillo@ryzen>
2018-12-20 22:54:58 +0100

    added mlflow

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,13 @@ FROM tensorflow/tensorflow:latest-gpu
 
 RUN apt-get -y install wget && \
     pip install -U jupyterlab keras livelossplot tables scikit-image tqdm && \
-    pip install -U dask[complete]
+    pip install -U dask[complete] && \
+    pip install -U mlflow
 
-CMD jupyter lab --allow-root
+# reminder that we expose port 5000 for MlFlow
+# Jupyter is exposing 8888
+EXPOSE 5000
+EXPOSE 8888
+
+CMD mlflow ui --file-store /notebooks/workspace/mlflow_server --host 0.0.0.0 --port 5000 & \
+    jupyter lab --allow-root
--- start_deep_learning_workspace.sh
+++ start_deep_learning_workspace.sh
@@ -1,4 +1,5 @@
-docker run --runtime=nvidia -it -p 8888:8888 \
+docker run --runtime=nvidia -it \
+-p 8888:8888 -p 5000:5000 \
 -v /home/pilillo/Documenti/deep_learning_workspace/workspace:/notebooks/workspace \
 --memory="60g" --memory-swap="60g" \
 pilillo/deep_learning_workspace:0.1
@@ -8,9 +9,3 @@ pilillo/deep_learning_workspace:0.1
 #--entrypoint bash \
 #--memory="60g" --memory-swap="60g" \
 #pilillo/deep_learning_workspace:0.1
-
-#docker run --runtime=nvidia -it -p 8888:8888 \
-#-v /home/pilillo/Documenti/deep_learning_workspace/workspace:/notebooks/workspace \
-#--entrypoint bash \
-#--memory="60g" --memory-swap="60g" \
-#tensorflow/tensorflow:latest-gpu
