6b7695e3164ed998517970d4cce34ef9f2811753
pilillo <pilillo@ryzen.local>
2019-02-19 13:35:20 +0100

    added python 3 env

--- build.sh
+++
@@ -1,1 +0,0 @@
-docker build -t pilillo/deep_learning_workspace:0.1 -f Dockerfile .
--- start_deep_learning_workspace.sh
+++ start_deep_learning_workspace.sh
@@ -1,11 +1,25 @@
-docker run --runtime=nvidia -it \
--p 8888:8888 -p 5000:5000 \
--v /home/pilillo/Documenti/deep_learning_workspace/workspace:/notebooks/workspace \
---memory="60g" --memory-swap="60g" \
-pilillo/deep_learning_workspace:0.1
+#!/usr/bin/env bash
 
-#docker run --runtime=nvidia -it -p 8888:8888 \
-#-v /home/pilillo/Documenti/deep_learning_workspace/workspace:/notebooks/workspace \
-#--entrypoint bash \
-#--memory="60g" --memory-swap="60g" \
-#pilillo/deep_learning_workspace:0.1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+tag="pilillo/deep_learning_workspace"
+version="0.1"
+
+if [ $# -eq 0 ]; then
+  echo "Please provide a python version 'v2' or 'v3'"
+  exit 1
+else
+  [[ $1 = "v2" ]] && t="${tag}:${version}" || t="${tag}-py3:${version}"
+fi
+
+docker run \
+--runtime=nvidia -it \
+-p 8888:8888 -p 5000:5000 \
+-v $DIR/workspace:/notebooks/workspace \
+--memory="50g" --memory-swap="50g" \
+-v $(which docker):/usr/bin/docker \
+-v /var/run/docker.sock:/var/run/docker.sock \
+$t || {
+  docker build -t $t -f $1/Dockerfile $1
+}
+#--entrypoint bash $t
